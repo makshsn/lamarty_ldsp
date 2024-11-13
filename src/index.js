b835cdcc9f98d602d5d38830b00865fe2c5de867
makshsn <maks.hoshin@mail.ru>
2024-11-13 10:24:33 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -16,9 +16,9 @@ const exportUsersToExcel = (users) => {
       user.name,
       "м2",
       Math.ceil(user.price),
-      Math.ceil((user.price / 10) * 1.4) * 10,
-      Math.ceil((user.price / 10) * 1.25) * 10,
-      Math.ceil((user.price / 10) * 1.25) * 1.78 * 10,
+      Math.ceil((user.price / 10) * 1.3) * 10,
+      Math.ceil((user.price / 10) * 1.3) * 10,
+      Math.ceil((user.price / 10) * 1.3) * 1.78 * 10,
     ];
   });
   exportExcel(data);
