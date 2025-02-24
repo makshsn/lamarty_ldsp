bb749ace3e8dd738c9e19e0403de870c999c97f4
makshsn <103880683+makshsn@users.noreply.github.com>
2025-02-24 18:02:10 +0300

    premium decors

--- src/variables.js
+++ src/variables.js
@@ -12,6 +12,12 @@ const DecorLuxWorkbook = XLSX.readFile("./src/price/LDSP_Decors_Lux.xlsx");
 const DecorLuxSheet_name = DecorLuxWorkbook.SheetNames;
 const DecorLuxXlData = XLSX.utils.sheet_to_json(DecorLuxWorkbook.Sheets[DecorLuxSheet_name[0]]);
 
+const DecorPremiumWorkbook = XLSX.readFile("./src/price/LDSP_Decors_Premium.xlsx");
+const DecorPremiumSheet_name = DecorPremiumWorkbook.SheetNames;
+const DecorPremiumXlData = XLSX.utils.sheet_to_json(
+  DecorPremiumWorkbook.Sheets[DecorPremiumSheet_name[0]]
+);
+
 let subCategoryFirst = [];
 let subCategorySecond = [];
 let subCategoryThird = [];
@@ -81,7 +87,60 @@ DecorLuxXlData.forEach((element) => {
     }
   }
 });
-console.log(luxFullName.length);
+
+DecorPremiumXlData.forEach((element) => {
+  if (element[10] != undefined) {
+    let slpited = element[10].split(",");
+    for (let n = 0; n < slpited.length; n++) {
+      premiumFullName.push({
+        Name: element["Премиум"],
+        Thickness: 10,
+        Subcategory: slpited[n],
+      });
+    }
+  }
+  if (element[16] != undefined) {
+    let slpited = element[16].split(",");
+    for (let n = 0; n < slpited.length; n++) {
+      premiumFullName.push({
+        Name: element["Премиум"],
+        Thickness: 16,
+        Subcategory: slpited[n],
+      });
+    }
+  }
+  if (element[18] != undefined) {
+    let slpited = element[18].split(",");
+    for (let n = 0; n < slpited.length; n++) {
+      premiumFullName.push({
+        Name: element["Премиум"],
+        Thickness: 18,
+        Subcategory: slpited[n],
+      });
+    }
+  }
+  if (element[22] != undefined) {
+    let slpited = element[22].split(",");
+    for (let n = 0; n < slpited.length; n++) {
+      premiumFullName.push({
+        Name: element["Премиум"],
+        Thickness: 22,
+        Subcategory: slpited[n],
+      });
+    }
+  }
+  if (element[26] != undefined) {
+    let slpited = element[26].split(",");
+    for (let n = 0; n < slpited.length; n++) {
+      premiumFullName.push({
+        Name: element["Премиум"],
+        Thickness: 26,
+        Subcategory: slpited[n],
+      });
+    }
+  }
+});
+console.log(premiumFullName);
 export {
   SubCategoryXlData,
   MaterialXlData,
